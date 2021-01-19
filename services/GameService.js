458a995508056134bee4e19830578b43db52adf5
Arevik <kivera16@gmail.com>
2021-01-19 15:03:13 +0400

    change

--- services/GameService.js
+++ services/GameService.js
@@ -15,28 +15,28 @@ class GameService {
         return sudokuBoard;
     }
 
-    getGameById(id) {
-        const sql = `SELECT * FROM game WHERE user_id=${id}`;
-        return queryAsync(sql)
-            .then((result) => result[0])
-            .catch((error) => console.log('error from  getGameById:', error));
-    }
+        getGameById(id) {
+            const sql = `SELECT * FROM game WHERE user_id=${id}`;
+            return queryAsync(sql)
+                .then((result) => result[0])
+                .catch((error) => console.log('error from  getGameById:', error));
+        }
 
-    insertNewGame(id, board) {
-        const json = JSON.stringify(board);
-        const sql = `INSERT game(user_id, sudoku) VALUES (${id}, ?)`;
-        return queryAsync(sql, [json])
-            .then(() => console.log('insert new game in db'))
-            .catch((error) => console.log('Error from insertNewGame:', error));
-    }
+        insertNewGame(id, board) {
+            const json = JSON.stringify(board);
+            const sql = `INSERT game(user_id, sudoku) VALUES (${id}, ?)`;
+            return queryAsync(sql, [json])
+                .then(() => console.log('insert new game in db'))
+                .catch((error) => console.log('Error from insertNewGame:', error));
+        }
 
-    updateGame(id, board) {
-        const json = JSON.stringify(board);
-        const sql = `UPDATE game SET sudoku = ? WHERE user_id=${id}`;
-        return queryAsync(sql, [json])
-            .then(() => console.log('update'))
-            .catch((error) => console.log('Error from updateGame:', error));
-    }
+        updateGame(id, board) {
+            const json = JSON.stringify(board);
+            const sql = `UPDATE game SET sudoku = ? WHERE user_id=${id}`;
+            return queryAsync(sql, [json])
+                .then(() => console.log('update'))
+                .catch((error) => console.log('Error from updateGame:', error));
+        }
 }
 
 module.exports = GameService;
